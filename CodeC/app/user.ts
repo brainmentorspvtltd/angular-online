3f506da20d076af4f74b715486259c6724b8ba19
brainmentorspvtltd <brainmentorspvtltd@gmail.com>
2018-06-26 17:23:49 +0530

    Add files via upload

---
+++ CodeC/app/user.ts
@@ -0,0 +1,11 @@
+export class User{   //model class
+    name:string;
+    contact:string;
+    email:string;
+    
+    constructor(name,contact,email){
+    this.name = name;
+    this.contact = contact;
+    this.email = email;
+    }
+    }
