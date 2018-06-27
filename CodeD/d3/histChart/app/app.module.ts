05ea6f4df5e752a7dccea1ab065882cf32c187a8
brainmentorspvtltd <brainmentorspvtltd@gmail.com>
2018-06-27 16:47:57 +0530

    Add files via upload

---
+++ CodeD/d3/histChart/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {NvD3Module} from 'ng2-nvd3';
+import 'd3';
+import 'nvd3';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,NvD3Module
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
